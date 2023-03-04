314c8f22e42438de2baf4a0178f68b70a1f66608
Akuna Joshua <akunajoshua@gmail.com>
2023-03-04 17:12:47 +0100

    part_3: Initial impl of the Model class
    Create a 4x4 matrice of Tile instances for the game in the Model class

--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -1,2 +1,17 @@
-class Model {
+/**
+ * The model class is responsible for all manipulations
+ * performed on the board.
+ */
+class Model() {
+    // defines the width of the board.
+    val FIELD_WIDTH: Int = 4
+
+    // define a 2-D array for the game Tiles.
+    private var gameTiles: Array<Array<Tile>>? = null
+        get() = field
+
+    init {
+        // Initializes the gameTiles reference attribute
+        gameTiles = Array(FIELD_WIDTH) { Array(FIELD_WIDTH) { Tile() } }
+    }
 }
